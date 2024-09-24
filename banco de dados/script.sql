773f3cd4a8d73a576f421cea4d9572c801f6f8ea
SaraSLeite <saraleite2708@gmail.com>
2024-09-23 21:53:14 -0300

    Reorganizando as imagens

--- banco de dados/script.sql
+++ banco de dados/script.sql
@@ -30,9 +30,9 @@ INSERT INTO produtos (nome, categoria_id, descricao, preco, quantidade, cor, ima
 ('Samsung Galaxy S24', (SELECT id FROM categorias WHERE nome = 'Smartphone'), 'Smartphone com tela Dynamic AMOLED 2X e câmera de 108 MP.', 899.99, 45, 'Azul', '/img/produtos/samsung.png'),
 ('Google Pixel 8', (SELECT id FROM categorias WHERE nome = 'Smartphone'), 'Telefone com câmeras avançadas e o novo processador Google Tensor.', 799.99, 40, 'Azul', '/img/produtos/google.png'),
 ('MacBook Air M2', (SELECT id FROM categorias WHERE nome = 'Notebook'), 'Notebook ultrafino da Apple com chip M2 e tela Retina.', 1199.99, 30, 'Prata', '/img/produtos/macbook.png'),
-('Dell XPS 13', (SELECT id FROM categorias WHERE nome = 'Notebook'), 'Laptop compacto com tela InfinityEdge e processador Intel Core i7.', 1299.99, 25, 'Prata', '/img/produtos/dell.jpg'),
-('Sony WH-1000XM5', (SELECT id FROM categorias WHERE nome = 'Fone de Ouvido'), 'Fones de ouvido com cancelamento de ruído ativo e qualidade de som premium.', 349.99, 20, 'Preto', '/img/produtos/sony.jpg'),
+('Dell XPS 13', (SELECT id FROM categorias WHERE nome = 'Notebook'), 'Laptop compacto com tela InfinityEdge e processador Intel Core i7.', 1299.99, 25, 'Prata', '/img/produtos/dell.png'),
+('Sony WH-1000XM5', (SELECT id FROM categorias WHERE nome = 'Fone de Ouvido'), 'Fones de ouvido com cancelamento de ruído ativo e qualidade de som premium.', 349.99, 20, 'Preto', '/img/produtos/sony.png'),
 ('Bose QuietComfort 45', (SELECT id FROM categorias WHERE nome = 'Fone de Ouvido'), 'Fones de ouvido com excelente cancelamento de ruído e conforto prolongado.', 329.99, 15, 'Preto', '/img/produtos/bose.png'),
 ('Canon EOS R5', (SELECT id FROM categorias WHERE nome = 'Câmera'), 'Câmera mirrorless com resolução de 45 MP e capacidade de gravação em 8K.', 3899.99, 10, 'Vermelho', '/img/produtos/canon.png'),
 ('Sony Bravia XR', (SELECT id FROM categorias WHERE nome = 'TV'), 'Smart TV 4K com tecnologia OLED e sistema de som integrado.', 2499.99, 20, 'Cinza', '/img/produtos/bravia.png'),
-('Samsung QLED 8K', (SELECT id FROM categorias WHERE nome = 'TV'), 'Televisor 8K QLED com qualidade de imagem superior e recursos inteligentes.', 3499.99, 15, 'Cinza', '/img/produtos/qled.jpg');
+('Samsung QLED 8K', (SELECT id FROM categorias WHERE nome = 'TV'), 'Televisor 8K QLED com qualidade de imagem superior e recursos inteligentes.', 3499.99, 15, 'Cinza', '/img/produtos/qled.png');
